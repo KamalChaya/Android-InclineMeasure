317f1d5a8ebb84318423b29acfb271ed43f21749
KamalChaya <thounumber1@gmail.com>
2014-11-06 21:51:36 -0800

    Added some more text

--- app/src/main/java/cs496/KamalChaya/inclinemeasure/app/MainActivity.java
+++ app/src/main/java/cs496/KamalChaya/inclinemeasure/app/MainActivity.java
@@ -1,3 +1,13 @@
+/*
+    Author: Kamal Chaya
+    Incline Measuring application for Android
+
+    The code to read the sensor values was adapted from here:
+        capycoding.blogspot.com/2012/10/get-angle-from-sensor-in-android.html
+
+
+ */
+
 package cs496.KamalChaya.inclinemeasure.app;
 
 import android.support.v7.app.ActionBarActivity;
@@ -6,7 +16,18 @@ import android.view.Menu;
 import android.view.MenuItem;
 
 
+
 public class MainActivity extends ActionBarActivity {
+
+    //Variables to hold the angles for the three axes of rotation
+    private float x = 0, y = 0, z = 0;
+
+    /*
+        array of floats to hold readings of accelerometer and
+        magnetic field sensor
+     */
+    private float[] accelReadings, magReadings;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
